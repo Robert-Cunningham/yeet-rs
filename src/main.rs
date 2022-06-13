cfc3666bbd7fccbeddaf3813eb1a6b49e1b1244a
Robert Cunningham <robertvcunningham@gmail.com>
2022-06-13 00:13:19 -0400

    Add procfs and get basic inode stuff working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Port to free up
+    port: u16,
+}
+
 fn main() {
-    println!("Yeet!");
+    let args = Args::parse();
+
+    let port = args.port;
+
+    let inodes: Vec<u64> = procfs::net::tcp()
+        .unwrap()
+        .into_iter()
+        .filter(|tcpentry| tcpentry.local_address.port() == port)
+        .map(|tcpentry| tcpentry.inode)
+        .collect();
+
+    /*
+    for p in procfs::net::tcp().unwrap() {
+        println!("{:?} -> {:?}", p.local_address.port(), p.inode)
+    }
+    */
+
+    for process in procfs::process::all_processes().unwrap() {
+        println!("{:?}", process.fd().unwrap());
+        for fd in process.fd().unwrap() {
+            match fd.target {
+                procfs::process::FDTarget::Socket(k) => println!("{}", k),
+                _ => println!("Yo"),
+            }
+        }
+    }
+
+    println!("Yeet {}", args.port);
 }
