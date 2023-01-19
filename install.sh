16c7bd84e1fd5ece4a5ac7441007fd21ae945230
Robert Cunningham <robertvcunningham@gmail.com>
2023-01-19 03:13:52 -0500

    add spaces to install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-	if [ "$(uname -m)" == "x86"]; then
+	if [ "$(uname -m)" == "x86" ]; then
 		sudo curl -fsSL https://github.com/Robert-Cunningham/yeet-rs/releases/download/v0.1.0/yeet-darwin-x64 > /usr/local/bin/yeet
 		chmod +x /usr/local/bin/yeet
-	elif ["$(uname -m)" == "arm64"]; then
+	elif [ "$(uname -m)" == "arm64" ]; then
 		sudo curl -fsSL https://github.com/Robert-Cunningham/yeet-rs/releases/download/v0.1.0/yeet-darwin-aarch64 > /usr/local/bin/yeet
 		chmod +x /usr/local/bin/yeet
 	fi
