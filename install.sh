c4d7e3d3afcf95237693ffddc6a0f6feac68b43b
Robert Cunningham <robertvcunningham@gmail.com>
2023-01-21 10:55:24 -0500

    add success echo to install script

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+
 version=0.2.3
 
 if [ "$(uname)" == "Darwin" ]; then
 	if [ "$(uname -m)" == "x86" ]; then
 		sudo curl -fsSL https://github.com/Robert-Cunningham/yeet-rs/releases/download/v${version}/yeet-darwin-x64 > /usr/local/bin/yeet
 		chmod +x /usr/local/bin/yeet
+		echo "Installed successfully. Go forth and yeet!"
 	elif [ "$(uname -m)" == "arm64" ]; then
 		sudo curl -fsSL https://github.com/Robert-Cunningham/yeet-rs/releases/download/v${version}/yeet-darwin-aarch64 > /usr/local/bin/yeet
 		chmod +x /usr/local/bin/yeet
+		echo "Installed successfully. Go forth and yeet!"
 	fi
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	sudo curl -fsSL https://github.com/Robert-Cunningham/yeet-rs/releases/download/v${version}/yeet-linux-x64 > /usr/local/bin/yeet
 	chmod +x /usr/local/bin/yeet
+	echo "Installed successfully. Go forth and yeet!"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
 	echo "yeet-rs does not support windows."
 fi
